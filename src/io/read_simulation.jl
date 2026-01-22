0e95bde58337b23ba724052dd82e1a01b32c2b9e
Jack Berat <jack.berat@laposte.net>
2026-01-22 14:01:20 +0100

    reorganisation

--- src/io/read_simulation.jl
+++ src/io/read_simulation.jl
@@ -1,5 +1,5 @@
 """
-    ReadSimulation(simu::String, LOS::String, conversionn::Number, conversionT::Number, conversionV::Number, conversionB::Number) 
+    ReadSimulation(simu::String, LOS::String, conversionn::Number, conversionT::Number, conversionV::Number, conversionB::Number)
         -> Tuple{AbstractArray, AbstractArray, AbstractArray, AbstractArray, AbstractArray, AbstractArray, AbstractArray, AbstractArray, Union{AbstractArray, Nothing}, Union{AbstractArray, Nothing}}
 
 Reads and processes simulation data from FITS files for a specified line of sight (LOS).
@@ -26,29 +26,15 @@ Reads and processes simulation data from FITS files for a specified line of sigh
   - `nHp::Union{AbstractArray, Nothing}`: Ionized hydrogen density array (or `nothing` if the file is not present).
 """
 function ReadSimulation(simu, LOS, conversionn, conversionT, conversionV, conversionB)
-    fileBx = "$simu/Bx.fits"
-    fileBy = "$simu/By.fits"
-    fileBz = "$simu/Bz.fits"
-    filen = "$simu/density.fits"
-    fileT = "$simu/temperature.fits"
-    fileVx = "$simu/Vx.fits"
-    fileVy = "$simu/Vy.fits"
-    fileVz = "$simu/Vz.fits"
-    filenH2 = "$simu/densityH2.fits"
-    filenHp = "$simu/densityHp.fits"
-    
-    T = read_file(fileT, conversionT)
-    n = read_file(filen, conversionn)
-    nH2 = read_optional_file(filenH2, conversionn, LOS)
-    nHp = read_optional_file(filenHp, conversionn, LOS)
+    paths = simulation_paths(simu)
 
-    B1, B2, BLOS = LOS == "z" ? (read_file(fileBx, conversionB), read_file(fileBy, conversionB), read_file(fileBz, conversionB)) :
-                    LOS == "y" ? (read_file(fileBz, conversionB), read_file(fileBx, conversionB), read_file(fileBy, conversionB)) :
-                                 (read_file(fileBy, conversionB), read_file(fileBz, conversionB), read_file(fileBx, conversionB))
+    T = read_file(paths.temperature, conversionT)
+    n = read_file(paths.density, conversionn)
+    nH2 = read_optional_file(paths.densityH2, conversionn, LOS)
+    nHp = read_optional_file(paths.densityHp, conversionn, LOS)
 
-    V1, V2, VLOS = LOS == "z" ? (read_file(fileVx, conversionV), read_file(fileVy, conversionV), read_file(fileVz, conversionV)) :
-                    LOS == "y" ? (read_file(fileVz, conversionV), read_file(fileVx, conversionV), read_file(fileVy, conversionV)) :
-                                 (read_file(fileVy, conversionV), read_file(fileVz, conversionV), read_file(fileVx, conversionV))
+    B1, B2, BLOS = read_magnetic_components(paths, LOS, conversionB)
+    V1, V2, VLOS = read_velocity_components(paths, LOS, conversionV)
 
     B1, B2, BLOS = permute_dims(B1, LOS), permute_dims(B2, LOS), permute_dims(BLOS, LOS)
     V1, V2, VLOS = permute_dims(V1, LOS), permute_dims(V2, LOS), permute_dims(VLOS, LOS)
@@ -58,25 +44,58 @@ function ReadSimulation(simu, LOS, conversionn, conversionT, conversionV, conver
 end
 
 function ReadSimulation(simu, LOS, conversionn, conversionT, conversionB)
-    fileBx = "$simu/Bx.fits"
-    fileBy = "$simu/By.fits"
-    fileBz = "$simu/Bz.fits"
-    filen = "$simu/density.fits"
-    fileT = "$simu/temperature.fits"
-    filenH2 = "$simu/densityH2.fits"
-    filenHp = "$simu/densityHp.fits"
-    
-    T = read_file(fileT, conversionT)
-    n = read_file(filen, conversionn)
-    nH2 = read_optional_file(filenH2, conversionn, LOS)
-    nHp = read_optional_file(filenHp, conversionn, LOS)
+    paths = simulation_paths(simu)
 
-    B1, B2, BLOS = LOS == "z" ? (read_file(fileBx, conversionB), read_file(fileBy, conversionB), read_file(fileBz, conversionB)) :
-                    LOS == "y" ? (read_file(fileBx, conversionB), read_file(fileBz, conversionB), read_file(fileBy, conversionB)) :
-                                 (read_file(fileBy, conversionB), read_file(fileBz, conversionB), read_file(fileBx, conversionB))
+    T = read_file(paths.temperature, conversionT)
+    n = read_file(paths.density, conversionn)
+    nH2 = read_optional_file(paths.densityH2, conversionn, LOS)
+    nHp = read_optional_file(paths.densityHp, conversionn, LOS)
+
+    B1, B2, BLOS = read_magnetic_components(paths, LOS, conversionB)
 
     B1, B2, BLOS = permute_dims(B1, LOS), permute_dims(B2, LOS), permute_dims(BLOS, LOS)
     T, n = permute_dims(T, LOS), permute_dims(n, LOS)
 
     return (B1, B2, BLOS, T, n, nH2, nHp)
+end
+
+function simulation_paths(simu)
+    return (
+        bx = "$simu/Bx.fits",
+        by = "$simu/By.fits",
+        bz = "$simu/Bz.fits",
+        density = "$simu/density.fits",
+        temperature = "$simu/temperature.fits",
+        vx = "$simu/Vx.fits",
+        vy = "$simu/Vy.fits",
+        vz = "$simu/Vz.fits",
+        densityH2 = "$simu/densityH2.fits",
+        densityHp = "$simu/densityHp.fits",
+    )
+end
+
+function read_magnetic_components(paths, LOS, conversionB)
+    Bx = read_file(paths.bx, conversionB)
+    By = read_file(paths.by, conversionB)
+    Bz = read_file(paths.bz, conversionB)
+    return select_components(LOS, Bx, By, Bz)
+end
+
+function read_velocity_components(paths, LOS, conversionV)
+    Vx = read_file(paths.vx, conversionV)
+    Vy = read_file(paths.vy, conversionV)
+    Vz = read_file(paths.vz, conversionV)
+    return select_components(LOS, Vx, Vy, Vz)
+end
+
+function select_components(LOS, comp_x, comp_y, comp_z)
+    if LOS == "z"
+        return (comp_x, comp_y, comp_z)
+    elseif LOS == "y"
+        return (comp_z, comp_x, comp_y)
+    elseif LOS == "x"
+        return (comp_y, comp_z, comp_x)
+    else
+        error("LOS must be x/y/z")
+    end
 end
