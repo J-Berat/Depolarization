ad610de72564aeafba6e2c077bd06a160540c99c
Jack Berat <119344245+J-Berat@users.noreply.github.com>
2026-01-22 15:11:04 +0100

    Rename read and write functions for FITS files

--- src/io/fits_io.jl
+++ src/io/fits_io.jl
@@ -1,10 +1,12 @@
 using FITSIO
 
-read_fits_array(path::AbstractString) = FITS(path, "r") do f
-    read(f[1])
+function read_FITS(path::AbstractString)
+    FITS(path, "r") do f
+        read(f[1])
+    end
 end
 
-function write_fits_array(path::AbstractString, data; overwrite::Bool=true)
+function write_FITS(path::AbstractString, data; overwrite::Bool=true)
     if overwrite && isfile(path)
         rm(path; force=true)
     end
